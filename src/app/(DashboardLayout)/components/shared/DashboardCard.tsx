89c8d0eac308c392c2b03e241071c82d5bb6e077
Aufaarib <aufa@nuncorp.id>
2024-01-04 16:58:33 +0700

    update status stepper

--- src/app/(DashboardLayout)/components/shared/DashboardCard.tsx
+++ src/app/(DashboardLayout)/components/shared/DashboardCard.tsx
@@ -1,6 +1,33 @@
-import React from "react";
-import { Card, CardContent, Typography, Stack, Box } from "@mui/material";
-import { IconBan, IconCircleCheck, IconClock } from "@tabler/icons-react";
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Stack,
+  Box,
+  Stepper,
+  Step,
+  StepLabel,
+  makeStyles,
+  withStyles,
+  styled,
+  StepConnector,
+  stepConnectorClasses,
+  StepIconProps,
+  StepIcon,
+} from "@mui/material";
+import {
+  IconBan,
+  IconCheck,
+  IconChecklist,
+  IconCircleCheck,
+  IconClearAll,
+  IconClock,
+  IconCross,
+  IconCrossFilled,
+  IconError404,
+  IconX,
+} from "@tabler/icons-react";
 
 type Props = {
   title?: string;
@@ -15,6 +42,10 @@ type Props = {
   status?: string;
 };
 
+interface CustomStepIconProps extends StepIconProps {
+  stepNumber: number;
+}
+
 const DashboardCard = ({
   title,
   subtitle,
@@ -27,6 +58,101 @@ const DashboardCard = ({
   middlecontent,
   status,
 }: Props) => {
+  const steps = ["Waiting", "Rejected", "Approved"];
+
+  const activeStep = () => {
+    if (status === "warning") {
+      return 0;
+    } else if (status === "rejected") {
+      return 1;
+    } else if (status === "approved") {
+      return 2;
+    }
+  };
+
+  const CustomStepIcon: React.FC<CustomStepIconProps> = ({
+    active,
+    stepNumber,
+  }) => {
+    // Customize each step's icon individually
+    return (
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        borderRadius="15px"
+        paddingX="50px"
+        width="60px"
+        color="white"
+        zIndex={1}
+      >
+        {stepNumber === 1 ? (
+          <Typography
+            display="flex"
+            flexDirection="row"
+            sx={
+              active
+                ? {
+                    borderRadius: "15px",
+                    paddingX: "10px",
+                    backgroundColor: "warning.main",
+                  }
+                : {
+                    borderRadius: "15px",
+                    paddingX: "10px",
+                    backgroundColor: "grey",
+                  }
+            }
+          >
+            Waiting <IconClock />
+          </Typography>
+        ) : stepNumber === 2 ? (
+          <Typography
+            display="flex"
+            flexDirection="row"
+            sx={
+              active
+                ? {
+                    borderRadius: "15px",
+                    paddingX: "10px",
+                    backgroundColor: "error.main",
+                  }
+                : {
+                    borderRadius: "15px",
+                    paddingX: "10px",
+                    backgroundColor: "grey",
+                  }
+            }
+          >
+            Rejected <IconBan />
+          </Typography>
+        ) : (
+          stepNumber === 3 && (
+            <Typography
+              display="flex"
+              flexDirection="row"
+              sx={
+                active
+                  ? {
+                      borderRadius: "15px",
+                      paddingX: "10px",
+                      backgroundColor: "success.main",
+                    }
+                  : {
+                      borderRadius: "15px",
+                      paddingX: "10px",
+                      backgroundColor: "grey",
+                    }
+              }
+            >
+              Approved <IconCircleCheck />
+            </Typography>
+          )
+        )}
+      </Box>
+    );
+  };
+
   return (
     <Card sx={{ padding: 0 }} elevation={9} variant={undefined}>
       {cardheading ? (
@@ -51,42 +177,61 @@ const DashboardCard = ({
                   <Box
                     sx={{
                       display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "space-between",
+                      flexDirection: "column",
                     }}
                   >
                     <Typography variant="h3">{title}</Typography>
                     {status ? (
-                      <Typography
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
-                        borderRadius="15px"
-                        padding="5px 15px"
-                        color="white"
-                        sx={
-                          status === "approved"
-                            ? { backgroundColor: "success.main" }
-                            : status === "rejected"
-                            ? { backgroundColor: "error.main" }
-                            : { backgroundColor: "warning.main" }
-                        }
+                      <Stepper
+                        sx={{ padding: "15px" }}
+                        activeStep={activeStep()}
+                        alternativeLabel
                       >
-                        {status === "approved" ? (
-                          <>
-                            Approved <IconCircleCheck />
-                          </>
-                        ) : status === "rejected" ? (
-                          <>
-                            Rejected <IconBan />
-                          </>
-                        ) : (
-                          <>
-                            Waiting <IconClock />
-                          </>
-                        )}
-                      </Typography>
+                        {steps.map((label, index) => (
+                          <Step key={label}>
+                            <StepLabel
+                              StepIconComponent={(props) => (
+                                <CustomStepIcon
+                                  stepNumber={index + 1}
+                                  {...props}
+                                />
+                              )}
+                            >
+                              {/* {label} */}
+                            </StepLabel>
+                          </Step>
+                        ))}
+                      </Stepper>
                     ) : (
+                      // <Typography
+                      //   display="flex"
+                      //   alignItems="center"
+                      //   justifyContent="center"
+                      //   borderRadius="15px"
+                      //   padding="5px 15px"
+                      //   color="white"
+                      //   sx={
+                      //     status === "approved"
+                      //       ? { backgroundColor: "success.main" }
+                      //       : status === "rejected"
+                      //       ? { backgroundColor: "error.main" }
+                      //       : { backgroundColor: "warning.main" }
+                      //   }
+                      // >
+                      //   {status === "approved" ? (
+                      //     <>
+                      //       Approved <IconCircleCheck />
+                      //     </>
+                      //   ) : status === "rejected" ? (
+                      //     <>
+                      //       Rejected <IconBan />
+                      //     </>
+                      //   ) : (
+                      //     <>
+                      //       Waiting <IconClock />
+                      //     </>
+                      //   )}
+                      // </Typography>
                       ""
                     )}
                   </Box>
